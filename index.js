fef5f729c159bac43068f27e57dbc77735248e49
YY <z3388638@gmail.com>
2018-07-21 19:32:43 +0800

    use node module

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Crawler = require('./Crawler');
+const CalendarGraph = require('github-calendar-graph');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,18 +12,18 @@ app.use(bodyParser.json());
 
 app.get('/graph/:account', cors(), (req, res) => {
 	const { account } = req.params;
-	Crawler.fetch(account).then((graph) => {
+	CalendarGraph.fetch(account).then((graph) => {
 		res.send(graph);
 	});
 });
 
 app.get('/data/:account', cors(), (req, res) => {
 	const { account } = req.params;
-	Crawler.fetch(account, true).then((data) => {
+	CalendarGraph.fetch(account, true).then((data) => {
 		res.json(data)
 	});
 });
 
 http.listen(7774, function () {
-	console.log('listening on 127.0.0.1:7774');
+	console.log('listening on http://127.0.0.1:7774');
 });
