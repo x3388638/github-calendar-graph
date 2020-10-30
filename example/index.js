7037d9af1f8583a79f999a13ac52146e55dea9aa
YY <z3388638@gmail.com>
2020-10-30 12:04:06 +0800

    Update example for json response version

--- example/index.js
+++ example/index.js
@@ -4,10 +4,19 @@ const GithubCalendarGraph = require('../index')
 
 app.get('/:account', async (req, res) => {
   const { account } = req.params
+  const { json } = req.query
   const graph = await GithubCalendarGraph.fetch(account)
-  res.send(graph)
+  if (json) {
+    res.json({
+      graph
+    })
+  } else {
+    res.send(graph)
+  }
 })
 
 app.listen('8080', () => {
-  console.log('Server listens on port 8080; try http://localhost:8080/x3388638')
+  console.log(
+    'Server listens on port 8080; try http://localhost:8080/x3388638 or http://localhost:8080/x3388638?json=true'
+  )
 })
